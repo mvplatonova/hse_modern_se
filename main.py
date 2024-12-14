74a14e7d76351a31981cd248fac0809b6bc39cfa
Marja Platonova <marja.platonova@gmail.com>
2024-12-14 14:37:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-
+from typing import Optional
 from fastapi import FastAPI, status
 
 from fastapi.responses import Response
 
 from model.dog import Dog
-from model.dog_type import DogType
 import service
 
 app = FastAPI()
-
 
 @app.get('/')
 def root():
@@ -16,55 +14,44 @@ def root():
 
 @app.post('/')
 def get_post_post():
-    return Response(status_code = status.HTTP_200_OK,
-                    content = service.create_timestamp())
+    return service.create_timestamp()
 
 @app.get('/dog')
-def get_dogs(type: DogType):
-    dogs = service.get_dogs_by_type(type)
+def get_dogs(kind: Optional[str] = None):
+    if (kind is None):
+        return service.list_dogs()
+    dogs = service.get_dogs_by_kind(kind)
     if len(dogs) == 0:  
         # if type not found
-        return Response(status_code = status.HTTP_422_UNPROCESSABLE_ENTITY_NOT_FOUND)
+        return Response(status_code = status.HTTP_422_UNPROCESSABLE_ENTITY)
     else:
-        return Response(
-                status_code = status.HTTP_200_OK,
-                content = dogs
-        )
+        return dogs
 
 @app.post('/dog')
 def create_dogs(dog: Dog):
     created_dog = service.create_dog(dog)
     if (created_dog is None):  
         # if dog with id has already exist
-        return Response(status_code = status.HTTP_422_UNPROCESSABLE_ENTITY_NOT_FOUND)
+        return Response(status_code = status.HTTP_422_UNPROCESSABLE_ENTITY)
     else:
-        return Response(
-                status_code=status.HTTP_200_OK,
-                content = created_dog
-        )
+        return created_dog
 
-@app.get('/dog/{id}')
-def get_dog_by_pk(dog_id: int):
-    found_dog = service.get_dog_by_id(dog_id)
+@app.get('/dog/{pk}')
+def get_dog_by_pk(pk: int):
+    found_dog = service.get_dog_by_pk(pk)
     if found_dog is None:  
         # if dog not found
         return Response(
-                status_code = status.HTTP_422_UNPROCESSABLE_ENTITY_NOT_FOUND
+                status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
         )
     else:
-        return Response(
-                status_code = status.HTTP_200_OK, 
-                content = found_dog
-        )
+        return found_dog
 
-@app.patch('/dog/{id}')
-def update_dogs(dog: Dog):
-    updated_dog = service.update_dog(dog)
+@app.patch('/dog/{pk}')
+def update_dogs(pk: int, dog: Dog):
+    updated_dog = service.update_dog(pk, dog)
     if (updated_dog is None):  
         # if dog with id has not exist
-        return Response(status_code = status.HTTP_422_UNPROCESSABLE_ENTITY_NOT_FOUND)
+        return Response(status_code = status.HTTP_422_UNPROCESSABLE_ENTITY)
     else:
-        return Response(
-                status_code=status.HTTP_200_OK,
-                content = updated_dog
-        )
+        return updated_dog
